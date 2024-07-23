df9b47d968e0f9b6aae03b96cf320bd3c7d95504
armacomiguel <armacomiguel@gmail.com>
2024-07-23 10:37:18 -0700

    first commit

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,49 @@
+'use server';
+
+import {nanoid} from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({userId, email}: CreateDocumentParams) => {
+    const roomId = nanoid();
+
+    try {
+        const metadata = {
+            creatorId: userId,
+            email,
+            title: 'Sin titulo'
+        }
+
+        const usersAccesses: RoomAccesses = {
+            [email]: ['room:write']
+        }
+
+        const room = await liveblocks.createRoom(roomId, {
+            metadata,
+            usersAccesses,
+            defaultAccesses: ['room:write']
+        });
+
+        revalidatePath('/');
+        return parseStringify(room);
+    } catch (error) {
+        console.log(`Error mientras se crea la sala: ${error}`);
+    }
+}
+
+export const getDocument = async ({ roomId, userId }: { roomId: string; userId: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+      
+        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+      
+        // if(!hasAccess) {
+        //   throw new Error('You do not have access to this document');
+        // }
+      
+        return parseStringify(room);
+    } catch (error) {
+      console.log(`Error happened while getting a room: ${error}`);
+    }
+  }
